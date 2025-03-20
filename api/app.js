f561beba39d6b3936b8d2baf19fa563eeb960f79
dysanchez <dylan.sr2002@gmail.com>
2025-03-20 15:23:14 -0500

    Public url

--- api/app.js
+++ api/app.js
@@ -3,13 +3,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const { CLIENT_URL } = require("./src/config/config");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: CLIENT_URL,
     credentials: true,
 }));
 app.use(cookieParser());
